00087ca06f1873ce37d44825e09a88503ba4bed6
av7836 <jkconstructionsweb@gmail.com>
2026-05-05 14:59:07 +0530

    db update

--- src/app/api/pages/route.ts
+++ src/app/api/pages/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+export const dynamic = "force-dynamic";
 import connectToDatabase from '@/lib/db';
 import { Page } from '@/models/Page';
 import fs from 'fs';
